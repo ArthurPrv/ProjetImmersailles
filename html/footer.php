0bfc01fbbdb1af9bca3956cc8b0800f13d6a006b
33782 <jdnijezhv>
2020-10-03 13:02:16 +0200

    Initial commit

---
+++ html/footer.php
@@ -0,0 +1,10 @@
+<div class="d-inline-block w-100 text-white bg-dark">
+    <h1 class="w-45  d-inline-block">
+        <img src="../images/logo_mini.png" class="w-auto">Immersailles </h1>
+    <h1 class="w-45 d-inline-block float-right">
+        <a href="Apropos.php" class="text-white">A propos</a>
+        <img src="../images/logo_mini.png" class="w-auto">
+        <img src="../images/logo_mini.png" class="w-auto">
+    </h1>
+
+</div>
